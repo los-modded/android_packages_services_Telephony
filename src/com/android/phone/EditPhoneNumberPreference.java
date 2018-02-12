f28264d9a48749032831ba57007b8109c479a0e9
Asher Simonds <dayanhammer@gmail.com>
2018-02-12 10:41:41 +0100

    Telephony: adaptive icon
    Change-Id: Ice040bb2785e8e4e3828c6008121520586d269ac
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/phone/EditPhoneNumberPreference.java
+++ src/com/android/phone/EditPhoneNumberPreference.java
@@ -324,7 +324,7 @@ public class EditPhoneNumberPreference extends EditTextPreference
             }
         }
         // set the call icon on the title.
-        builder.setIcon(R.mipmap.ic_launcher_phone);
+        builder.setIcon(R.mipmap.ic_launcher);
     }
 
 
